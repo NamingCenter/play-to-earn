0c32182764cd02fef05d216f14fc92a2bedd3896
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2022-04-06 00:48:30 +0900

    fix code

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import { useEffect } from "react";
 import Routers from "./components/routes/Routers";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import { useDispatch, useSelector } from "react-redux";
-import { getWeb3, connectWallet, checkWallet } from "./redux/actions/index";
+import { useDispatch } from "react-redux";
+import { connect, getWeb3 } from "./redux/actions/index";
+import MetaMaskOnboarding from "@metamask/onboarding";
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getWeb3());
+    dispatch(connect());
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      dispatch(getWeb3());
+    }
   }, [dispatch]);
-
-  // const CreateNFTContract = useSelector((state) => state.AppState.CreateNFTContract);
   return (
     <>
       <Header />
