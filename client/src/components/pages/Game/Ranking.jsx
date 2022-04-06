dfbfa141810c242f0eaf25bbacb9eb0bf645c2df
이성현 <marvel97@naver.com>
2022-04-07 00:23:05 +0900

    myranking이랑 sidebar 다시 수정

--- client/src/components/pages/Game/Ranking.jsx
+++ client/src/components/pages/Game/Ranking.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Col, Container, Row } from "reactstrap";
 import CommonSection from "../../ui/templete/CommonSection";
 import "./ranking.css";
@@ -11,26 +11,6 @@ const Ranking = () => {
   const [toggleState, setToggleState] = useState(1);
   const account = useSelector((state) => state.AppState.account);
 
-  // Claim부분
-  const [snakeAddress, setSnakeAddress] = useState([]);
-  const [puzzleAddress, setPuzzleAddress] = useState([]);
-  const [tetrisAddress, setTetrisAddress] = useState([]);
-  const [mineAddress, setMineAddress] = useState([]);
-
-  const sendReward = async () => {
-    await axios
-      .post(`http://localhost:5000/ranking`, {
-        tetrisAddress,
-        puzzleAddress,
-        snakeAddress,
-        mineAddress,
-      })
-      .then((res) => {
-        console.log(res.data);
-        alert("DB 전송 완료");
-      });
-  };
-
   const [timerDays, setTimerDays] = useState();
   const [timerHours, setTimerHours] = useState();
   const [timerMinutes, setTimerMinutes] = useState();
@@ -38,132 +18,94 @@ const Ranking = () => {
 
   // const [isStop, setIsStop] = useState(false);
 
-  useEffect(() => {
-    let interval = setInterval(() => {
-      const countdownDate = new Date("apr 15, 2022 18:00:00").getTime();
+  //   useEffect(() => {
+  //     let interval = setInterval(() => {
+  //       const countdownDate = new Date("apr 15, 2022 18:00:00").getTime();
+  //       // var weeks = new Date(now.getDate() + 7);
 
-      const now = new Date().getTime();
-      const distance = countdownDate - now;
+  //       const now = new Date().getTime();
+  //       const distance = countdownDate - now;
 
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      if (!isStop) {
-        // update timer
-        setTimerDays(days);
-        setTimerHours(hours);
-        setTimerMinutes(minutes);
-        setTimerSeconds(seconds);
-      } else {
-        clearInterval(interval);
-      }
-    }, 1000);
-    return () => {
-      setIsStop(true);
-    };
-  }, []);
+  //       const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //       const hours = Math.floor(
+  //         (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //       );
+  //       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //       if (!isStop) {
+  //         // update timer
+  //         setTimerDays(days);
+  //         setTimerHours(hours);
+  //         setTimerMinutes(minutes);
+  //         setTimerSeconds(seconds);
+  //       } else {
+  //         clearInterval(interval);
+  //       }
+  //     }, 1000);
+  //     return () => {
+  //       setIsStop(true);
+  //     };
+  //   }, []);
 
   const toggleTab = (index) => {
     setToggleState(index);
   };
 
-  // 변수 뒤에 T => 배열의 인덱스
-  // 변수 뒤에 I => 받은 데이터의 배열
-  const [snake, setSnake] = useState([]);
-  const [snakeT, setSnakeT] = useState(null);
-  const [snakeI, setSnakeI] = useState([]);
-
-  const [puzzle, setPuzzle] = useState([]);
-  const [puzzleT, setPuzzleT] = useState(null);
-  const [puzzleI, setPuzzleI] = useState([]);
-
-  const [mine, setMine] = useState([]);
-  const [mineT, setMineT] = useState(null);
-  const [mineI, setMineI] = useState([]);
-
-  const [tetris, setTetris] = useState([]);
-  const [tetrisT, setTetrisT] = useState(null);
-  const [tetrisI, setTetrisI] = useState([]);
-
+  const [rankingDB, setRankingDB] = useState(null);
+  const [weekly, setWeekly] = useState(null);
+  console.log("w", weekly);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5000/game/snake`)
-      .then((response) => {
-        const data = response.data;
-        setSnake(data);
-
-        const snakeIndex = data.findIndex((element) => {
-          if (element.address === account) {
-            setSnakeI(element);
-            return true;
-          }
+    if (account !== null) {
+      axios
+        .post(`http://localhost:5000/game/ranking`, { address: account })
+        .then((response) => {
+          const data = response.data;
+          setRankingDB(data);
+        })
+        .catch((error) => {
+          setError(error);
         });
-        setSnakeT(snakeIndex);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-
-    axios
-      .get(`http://localhost:5000/game/2048`)
-      .then((response) => {
-        const data = response.data;
-        setPuzzle(data);
-
-        const puzzleIndex = data.findIndex((element) => {
-          if (element.address === account) {
-            setPuzzleI(element);
-            return true;
-          }
-        });
-        setPuzzleT(puzzleIndex);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-
-    axios
-      .get(`http://localhost:5000/game/mine`)
-      .then((response) => {
-        const data = response.data;
-        setMine(data);
-
-        const mineIndex = data.findIndex((element) => {
-          if (element.address === account) {
-            setMineI(element);
-            return true;
-          }
-        });
-        setMineT(mineIndex);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-
-    axios
-      .get(`http://localhost:5000/game/tetris`)
-      .then((response) => {
-        const data = response.data;
-        setTetris(data);
-
-        const tetrisIndex = data.findIndex((element) => {
-          if (element.address === account) {
-            setTetrisI(element);
-            return true;
-          }
-        });
-        setTetrisT(tetrisIndex);
-      })
-      .catch((error) => {
-        setError(error);
-      });
+    }
     setLoading(false);
+  }, [account]);
+
+  useEffect(() => {
+    axios
+      .post(`http://localhost:5000/game/weekly`)
+      .then((response) => {
+        const data = response.data;
+        setWeekly(data);
+      })
+      .catch((error) => {
+        setError(error);
+      });
   }, []);
+
+  function RankingListForm(form) {
+    const result = [];
+    for (let i = 0; i < 3; i++) {
+      if (form[i] === undefined) {
+        // console.log("위쪽");
+        result.push(
+          <Fragment key={i}>
+            <p>{i + 1}등 : 바로당신의 자리 </p>
+          </Fragment>
+        );
+      } else {
+        // console.log("아래쪽");
+        result.push(
+          <Fragment key={i}>
+            <p>
+              {i + 1}등 : {form[i].nick}
+            </p>
+          </Fragment>
+        );
+      }
+    }
+    return result;
+  }
 
   return (
     <>
@@ -208,105 +150,27 @@ const Ranking = () => {
                       <ul>
                         <li>SnakeGame</li>
                         <br />
-                        {snake
-                          .filter((v, i) => {
-                            return i < 3;
-                          })
-                          .map((v, i) => {
-                            return (
-                              <p key={i}>
-                                {i + 1}등 :&nbsp;
-                                {v === undefined
-                                  ? "없음"
-                                  : v.snakePoint === null
-                                  ? "없음"
-                                  : v.nick}
-                                &nbsp;
-                                {v === undefined
-                                  ? ""
-                                  : v.snakePoint === null
-                                  ? ""
-                                  : v.snakePoint + "점"}
-                              </p>
-                            );
-                          })}
-
+                        {rankingDB !== null
+                          ? RankingListForm(rankingDB.snakeranker)
+                          : false}
                         <br />
                         <li>TetrisGame</li>
                         <br />
-                        {tetris
-                          .filter((v, i) => {
-                            return i < 3;
-                          })
-                          .map((v, i) => {
-                            return (
-                              <p key={i}>
-                                {i + 1}등 :&nbsp;
-                                {v === undefined
-                                  ? "없음"
-                                  : v.tetrisPoint === null
-                                  ? "없음"
-                                  : v.nick}
-                                &nbsp;
-                                {v === undefined
-                                  ? ""
-                                  : v.tetrisPoint === null
-                                  ? ""
-                                  : v.tetrisPoint + "점"}
-                              </p>
-                            );
-                          })}
-
+                        {rankingDB !== null
+                          ? RankingListForm(rankingDB.tetrisranker)
+                          : false}
                         <br />
                         <li>2048Game</li>
                         <br />
-                        {puzzle
-                          .filter((v, i) => {
-                            return i < 3;
-                          })
-                          .map((v, i) => {
-                            return (
-                              <p key={i}>
-                                {i + 1}등 :&nbsp;
-                                {v === undefined
-                                  ? "없음"
-                                  : v.puzzlePoint === null
-                                  ? "없음"
-                                  : v.nick}
-                                &nbsp;
-                                {v === undefined
-                                  ? ""
-                                  : v.puzzlePoint === null
-                                  ? ""
-                                  : v.puzzlePoint + "점"}
-                              </p>
-                            );
-                          })}
+                        {rankingDB !== null
+                          ? RankingListForm(rankingDB.puzzleranker)
+                          : false}
                         <br />
                         <li>MineGame</li>
                         <br />
-                        {mine
-                          .filter((v, i) => {
-                            return i < 3;
-                          })
-                          .map((v, i) => {
-                            return (
-                              <p key={i}>
-                                {i + 1}등 :&nbsp;
-                                {v === undefined
-                                  ? "없음"
-                                  : v.minePoint === null
-                                  ? ""
-                                  : v.nick}
-                                &nbsp;
-                                {v === undefined
-                                  ? ""
-                                  : v.minePoint === null
-                                  ? ""
-                                  : v.minePoint + "초"}
-                              </p>
-                            );
-                          })}
+                        {rankingDB !== null
+                          ? RankingListForm(rankingDB.mineranker)
+                          : false}
                       </ul>
                     </div>
                   </Container>
@@ -321,7 +185,6 @@ const Ranking = () => {
                   <hr />
                   <Container>
                     <div className="ranking__box">
-                      여기에 주간랭킹 순위표만들기
                     </div>
                   </Container>
                 </div>
@@ -337,23 +200,43 @@ const Ranking = () => {
                     <ul>
                       <li>SnakeGame</li>
                       <br />
-                      {snakeI.snakePoint !== null ? snakeT + 1 + "등" : "없음"}
+                      <p>
+                        {rankingDB !== null
+                          ? rankingDB.snakeMyRanking === 0
+                            ? "순위없음"
+                            : rankingDB.snakeMyRanking + " 등"
+                          : false}
+                      </p>
                       <br />
                       <li>2048Game</li>
                       <br />
-                      {puzzleI.puzzlePoint !== null
-                        ? puzzleT + 1 + "등"
-                        : "없음"}
+                      <p>
+                        {rankingDB !== null
+                          ? rankingDB.puzzleMyRanking === 0
+                            ? "순위없음"
+                            : rankingDB.puzzleMyRanking + " 등"
+                          : false}
+                      </p>
                       <br />
                       <li>TetrisGame</li>
                       <br />
-                      {tetrisI.tetrisPoint !== null
-                        ? tetrisT + 1 + "등"
-                        : "없음"}
+                      <p>
+                        {rankingDB !== null
+                          ? rankingDB.tetrisMyRanking === 0
+                            ? "순위없음"
+                            : rankingDB.tetrisMyRanking + " 등"
+                          : false}
+                      </p>
                       <br />
                       <li>MineGame</li>
                       <br />
-                      {mineI.minePoint !== null ? mineT + 1 + "등" : "없음"}
+                      <p>
+                        {rankingDB !== null
+                          ? rankingDB.mineMyRanking === 0
+                            ? "순위없음"
+                            : rankingDB.mineMyRanking + " 등"
+                          : false}
+                      </p>
                     </ul>
                   </Container>
                 </div>
@@ -368,9 +251,6 @@ const Ranking = () => {
                 timerMinutes={timerMinutes}
                 timerSeconds={timerSeconds}
               />
-              <div type="button" onClick={sendReward}>
-                Claim All Reward!!
-              </div>
             </Col>
           </Row>
         </div>
