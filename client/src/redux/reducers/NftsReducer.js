84919c8392b4ef79356fbc21c9053d19c2c3ce9d
이성현 <marvel97@naver.com>
2022-04-08 12:19:43 +0900

    mynftlist 점수등록부분에 가져옴

--- client/src/redux/reducers/NftsReducer.js
+++ client/src/redux/reducers/NftsReducer.js
@@ -1,35 +1,23 @@
 import { SET_NFTS, SELECTED_NFT } from "../actions";
 
 const initialState = {
-    name: null,
-    description: null,
-    image: null,
+  name: null,
+  description: null,
+  image: null,
 };
 
 export default function (state = initialState, action) {
-    switch (action.type) {
-        case SET_NFTS:
-            return {
-                ...initialState,
-                name: action.payload.name,
-                description: action.payload.description,
-                image: action.payload.image,
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_NFTS:
+      return {
+        ...initialState,
+        name: action.payload.name,
+        description: action.payload.description,
+        image: action.payload.image,
+      };
+    default:
+      return state;
+  }
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-// NftReducer.js 수정
-=======
-// NftReducer.js 수정
->>>>>>> 4ca3e93 (Update NftsReducer.js)
-=======
-// git commit
->>>>>>> 8d54a32 (nftReducer / upperCase)
-=======
-// git commit
->>>>>>> 2682a65 (fix name)
+
