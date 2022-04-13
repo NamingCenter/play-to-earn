eeb93cadf61db0ed91c31be45be7d25e697b3859
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2022-04-13 21:40:04 +0900

    view mybalance

--- client/src/components/ui/dashboard/Earnings.jsx
+++ client/src/components/ui/dashboard/Earnings.jsx
@@ -1,45 +1,23 @@
-import React, { useEffect, useState } from "react";
-import "./earning.css";
-import axios from "axios";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import "./earning.css";
 
 const Earnings = () => {
-  const [token, setToken] = useState("");
-  const [totalAAK, setTotalAAK] = useState("");
-  const TokenClaimContract = useSelector(
-    (state) => state.AppState.TokenClaimContract
-  );
-  const account = useSelector((state) => state.AppState.account);
-
-  useEffect(async () => {
-    if (account !== null) {
-      mybalance();
-    }
-  }, [account]);
-
-  const mybalance = async () => {
-    if (TokenClaimContract !== null) {
-      const result = await TokenClaimContract.methods
-        .mybalance()
-        .call({ from: account });
-      return setTotalAAK(result);
-    } else {
-      alert("컨트랙트 로드 실패!!\n네트워크를 확인하세요");
-    }
-  };
-  return (
-    <div className="earning__card">
-      <div className="card__content">
-        <div className="earning__chart">
-          <i className="ri-copper-diamond-line"></i>
+    const [token, setToken] = useState("00.00");
+    const Mybalance = useSelector((state) => state.AppState.Mybalance);
+    return (
+        <div className="earning__card">
+            <div className="card__content">
+                <div className="earning__chart">
+                    <i className="ri-copper-diamond-line"></i>
+                </div>
+                <div className="earing__text">
+                    <div className="token__mybox">{Mybalance} ETH</div>
+                    <div className="token__mydesc">Total Volume</div>
+                </div>
+            </div>
         </div>
-        <div className="earing__text">
-          <div className="token__mybox">{totalAAK} AAT</div>
-          <div className="token__mydesc">Total Volume</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Earnings;
