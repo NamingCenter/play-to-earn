0d2724341ea6798839b5deb2854f8cf290efcc24
Hyerin1208 <fghjiff8@gmail.com>
2022-04-21 09:38:23 +0900

    clone

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,49 +1,35 @@
 var express = require("express");
+const path = require("path");
 var router = express.Router();
 const User = require("../models/user");
 
 // 회원정보 등록
 router.post("/register", async (req, res, next) => {
-  // console.log(req.body);
   const { nick, email, address, image } = req.body;
 
-  const alreadyExistsEmail = await User.findOne({ where: { email } }).catch(
+  const alreadyExistsUser = await User.findOne({ where: { email } }).catch(
     (err) => {
       console.log("Error: ", err);
     }
   );
 
-  const alreadyExistsNick = await User.findOne({ where: { nick } }).catch(
-    (err) => {
-      console.log("Error: ", err);
-    }
-  );
-
-  if (alreadyExistsNick) {
-    return res.json({ message: "닉네임이 이미 존재합니다!", bool: false });
+  if (alreadyExistsUser) {
+    return res.json({ message: "User with email already exists!" });
   }
-  if (alreadyExistsEmail) {
-    return res.json({ message: "이메일이 이미 존재합니다!", bool: false });
-  }
-
-  console.log("이미지" + image);
 
   const newUser = new User({ nick, email, address, image });
 
   if (image == false) {
-    console.log(1);
     image = "../../client/src/assets/images/img.jpg";
   }
-  console.log(image);
 
   const savedUser = await newUser.save().catch((err) => {
     console.log("Error: ", err);
-    res.json({ error: "회원가입을 할 수 없습니다!", bool: true });
+    res.json({ error: "Cannot register user at the moment!" });
   });
 
-  if (savedUser)
-    res.json({ message: "회원가입 해주셔서 감사합니다", bool: true });
-  else res.json({ error: "회원가입을 할 수 없습니다!", bool: true });
+  if (savedUser) res.json({ message: "Thanks for registering" });
+  // else res.json({ error: "Cannot register user at the moment!" });
 });
 
 // 회원정보 불러오기
@@ -56,8 +42,6 @@ router.post("/login", async (req, res, next) => {
       where: { address: address },
       attributes: ["nick", "email", "image"],
     });
-
-    // console.log(users);
 
     if (!users) {
       const login = { nick: "noname", email: "no-email" };
@@ -75,12 +59,18 @@ router.post("/login", async (req, res, next) => {
 
 /* 프로필 IMG CREATE, edit */
 router.post("/img", async (req, res) => {
-  console.log(req.body.image);
   const { image, address } = req.body;
 
   try {
     if (image) {
-      await User.update({ image }, { where: { address: address } });
+      await User.update(
+        {
+          image,
+        },
+        {
+          where: { address: address },
+        }
+      );
       res.json({ message: "ok" });
     } else {
       res.json({ message: "no" });
@@ -93,13 +83,20 @@ router.post("/img", async (req, res) => {
 
 // 회원정보 수정
 router.post("/edit", async (req, res) => {
-  console.log(req.body);
-
   const { nick, email, address } = req.body;
 
   try {
     if (nick && email) {
-      await User.update({ nick, email }, { where: { address: address } });
+      await User.update(
+        {
+          nick,
+          email,
+        },
+
+        {
+          where: { address: address },
+        }
+      );
       res.json({ message: "ok" });
     } else {
       res.json({ message: "no" });
@@ -127,16 +124,13 @@ router.post("/weeks", async (req, res) => {
 
 // 오너체크
 router.post("/owner", async (req, res, next) => {
-  const { count } = req.body;
   try {
     const newDate = new Date().getTime() + 604800000;
 
-    console.log(newDate);
     const address = req.body.address;
     const owner = await User.findOne({ where: { address: address } });
-    console.log(address);
     if (owner) {
-      res.json({ message: "운영자 맞아요" });
+      res.json({ message: "운영자 맞아요", count: owner.count });
     } else {
       await User.create({
         address: address,
@@ -145,7 +139,10 @@ router.post("/owner", async (req, res, next) => {
         email: "Owner@gmail.com",
         count: newDate.toString(),
       });
-      res.json({ message: "첫번째 실행으로 운영자 계정만들어요" });
+      res.json({
+        message: "첫번째 실행으로 운영자 계정만들어요",
+        count: newDate.toString(),
+      });
     }
   } catch (err) {
     console.error(err);
