5a86efbec87038965df7fe286a6a478d46a4d045
Hyerin1208 <fghjiff8@gmail.com>
2022-04-25 11:42:51 +0900

    page title + css

--- client/src/components/pages/Staking/Card.jsx
+++ client/src/components/pages/Staking/Card.jsx
@@ -49,7 +49,7 @@ function CompactCard({ param, setExpanded }) {
       </div>
       <div className="detail">
         <Png />
-        <span>${param.value}</span>
+        <p>{param.value} AAT</p>
         <span>Last 24 hours</span>
       </div>
     </motion.div>
