04b7cec1acd1b0e3ca4f7c02c41dae490177e97a
coolmarvel <marvel97@naver.com>
2022-04-29 11:49:42 +0900

    update

--- client/src/components/pages/Game/Ranking.jsx
+++ client/src/components/pages/Game/Ranking.jsx
@@ -64,24 +64,22 @@ const Ranking = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (account !== null) {
-      axios
-        .post(`http://15.165.17.43:5000/game/ranking`, { address: account })
-        .then((response) => {
-          const data = response.data;
-          setRankingDB(data);
-        })
-        .catch((error) => {
-          setError(error);
-          window.location.href = "/error";
-        });
-    }
+    axios
+      .post(`http://localhost:5000/game/ranking`, { address: account })
+      .then((response) => {
+        const data = response.data;
+        setRankingDB(data);
+      })
+      .catch((error) => {
+        setError(error);
+        window.location.href = "/error";
+      });
     setLoading(false);
   }, [account]);
 
   useEffect(() => {
     axios
-      .post(`http://15.165.17.43:5000/game/weekly`)
+      .post(`http://localhost:5000/game/weekly`)
       .then((response) => {
         const data = response.data;
         const sortData = data.map((v, i) => {
